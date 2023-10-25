e8a67ea6e05147aba650fe270ee301d0d018cea9
VivekBira <88651056+VivekBira@users.noreply.github.com>
2023-10-25 12:34:34 +0530

    Update HarnessApplication.java

--- src/main/java/com/example/Harness/HarnessApplication.java
+++ src/main/java/com/example/Harness/HarnessApplication.java
@@ -8,7 +8,7 @@ public class HarnessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HarnessApplication.class, args);
-		System.out.println("we are running the java code in harness environment);
+		System.out.println("we are running the java code in harness environment");
 	}
 
 }
